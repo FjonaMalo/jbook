70c6f319d68315516e0c23dfee82f7abdc74fc0e
fjonamalo <fjonamalo0@gmail.com>
2024-01-16 13:24:32 +0100

    Basic Form Elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi there</div>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const handleClick = () => {
+    console.log("input", input);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 export default App;
